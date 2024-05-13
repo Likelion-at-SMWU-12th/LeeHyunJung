5d48656cd6401f62ee45435d5a756349f0ec843b
hyhy-j <lhjlhj10088@gmail.com>
2024-05-13 21:10:29 +0900

    css 변경, 별 찍기

--- front_week01/star.js
+++ front_week01/star.js
@@ -7,6 +7,18 @@ function isLeapYear(year) {
     return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 }
 
+function clickHandler(event){
+    const emoji = document.createElement("span");
+    emoji.innerHTML = "🌟";
+    // emoji.classList.add("emoji");
+
+    emoji.style.position = "absolute";
+    emoji.style.left = event.pageX + "px";
+    emoji.style.top = event.pageY + "px";
+
+    document.body.appendChild(emoji);
+}
+
 function loginSubmit(event){
     event.preventDefault();
     loginForm.classList.add("hidden");
@@ -16,38 +28,39 @@ function loginSubmit(event){
     const timeDiff = selectedDate.getTime() - new Date(selectedDate.getFullYear(),0,1).getTime();
     const daysDiff = Math.floor(timeDiff/(1000*60*60*24));
 
-    if (daysDiff <= 18) star.innerHTML = nameinput + "님의 별자리는 💫염소자리💫입니다.";
-    else if (daysDiff<=48) star.innerHTML = nameinput + "님의 별자리는 💫물병자리💫입니다.";
+    if (daysDiff <= 18) star.innerHTML = nameinput + "님의 별자리는 💫염소자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+    else if (daysDiff<=48) star.innerHTML = nameinput + "님의 별자리는 💫물병자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
     else{
         if (isLeapYear(selectedDate.getFullYear())){
-            if (daysDiff<=79) star.innerHTML = nameinput + "님의 별자리는 💫물고기자리💫입니다.";
-            else if (daysDiff<=110) star.innerHTML = nameinput + "님의 별자리는 💫양자리💫입니다.";
-            else if (daysDiff<=140) star.innerHTML = nameinput + "님의 별자리는 💫황소자리💫입니다.";
-            else if (daysDiff<=172) star.innerHTML = nameinput + "님의 별자리는 💫쌍둥이자리💫입니다.";
-            else if (daysDiff<=203) star.innerHTML = nameinput + "님의 별자리는 💫게자리💫입니다.";
-            else if (daysDiff<=234) star.innerHTML = nameinput + "님의 별자리는 💫사자자리💫입니다.";
-            else if (daysDiff<=265) star.innerHTML = nameinput + "님의 별자리는 💫처녀자리💫입니다.";
-            else if (daysDiff<=294) star.innerHTML = nameinput + "님의 별자리는 💫천칭자리💫입니다.";
-            else if (daysDiff<=325) star.innerHTML = nameinput + "님의 별자리는 💫전갈자리💫입니다.";
-            else if (daysDiff<=355) star.innerHTML = nameinput + "님의 별자리는 💫사수자리💫입니다.";
-            else star.innerHTML = nameinput + "님의 별자리는 💫염소자리💫입니다.";
+            if (daysDiff<=79) star.innerHTML = nameinput + "님의 별자리는 💫물고기자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=110) star.innerHTML = nameinput + "님의 별자리는 💫양자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=140) star.innerHTML = nameinput + "님의 별자리는 💫황소자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=172) star.innerHTML = nameinput + "님의 별자리는 💫쌍둥이자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=203) star.innerHTML = nameinput + "님의 별자리는 💫게자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=234) star.innerHTML = nameinput + "님의 별자리는 💫사자자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=265) star.innerHTML = nameinput + "님의 별자리는 💫처녀자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=294) star.innerHTML = nameinput + "님의 별자리는 💫천칭자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=325) star.innerHTML = nameinput + "님의 별자리는 💫전갈자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=355) star.innerHTML = nameinput + "님의 별자리는 💫사수자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else star.innerHTML = nameinput + "님의 별자리는 💫염소자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
         }
         else{
-            if (daysDiff<=78) star.innerHTML = nameinput + "님의 별자리는 💫물고기자리💫입니다.";
-            else if (daysDiff<=109) star.innerHTML = nameinput + "님의 별자리는 💫양자리💫입니다.";
-            else if (daysDiff<=139) star.innerHTML = nameinput + "님의 별자리는 💫황소자리💫입니다.";
-            else if (daysDiff<=171) star.innerHTML = nameinput + "님의 별자리는 💫쌍둥이자리💫입니다.";
-            else if (daysDiff<=202) star.innerHTML = nameinput + "님의 별자리는 💫게자리💫입니다.";
-            else if (daysDiff<=233) star.innerHTML = nameinput + "님의 별자리는 💫사자자리💫입니다.";
-            else if (daysDiff<=264) star.innerHTML = nameinput + "님의 별자리는 💫처녀자리💫입니다.";
-            else if (daysDiff<=293) star.innerHTML = nameinput + "님의 별자리는 💫천칭자리💫입니다.";
-            else if (daysDiff<=324) star.innerHTML = nameinput + "님의 별자리는 💫전갈자리💫입니다.";
-            else if (daysDiff<=354) star.innerHTML = nameinput + "님의 별자리는 💫사수자리💫입니다.";
-            else star.innerHTML = nameinput + "님의 별자리는 💫염소자리💫입니다.";
+            if (daysDiff<=78) star.innerHTML = nameinput + "님의 별자리는 💫물고기자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=109) star.innerHTML = nameinput + "님의 별자리는 💫양자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=139) star.innerHTML = nameinput + "님의 별자리는 💫황소자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=171) star.innerHTML = nameinput + "님의 별자리는 💫쌍둥이자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=202) star.innerHTML = nameinput + "님의 별자리는 💫게자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=233) star.innerHTML = nameinput + "님의 별자리는 💫사자자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=264) star.innerHTML = nameinput + "님의 별자리는 💫처녀자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=293) star.innerHTML = nameinput + "님의 별자리는 💫천칭자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=324) star.innerHTML = nameinput + "님의 별자리는 💫전갈자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else if (daysDiff<=354) star.innerHTML = nameinput + "님의 별자리는 💫사수자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
+            else star.innerHTML = nameinput + "님의 별자리는 💫염소자리💫입니다.<br>클릭하여 당신의 밤하늘을 채워주세요.";
         }
     }
-
     star.classList.remove("hidden");
+
+    document.addEventListener("click", clickHandler);
 }
 
 loginForm.addEventListener("submit", loginSubmit);
