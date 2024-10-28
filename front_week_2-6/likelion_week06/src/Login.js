606202aa9670d6b680dcaf3cf7b0d824854a5ac4
hyhy-j <lhjlhj10088@gmail.com>
2024-10-28 15:42:49 +0900

    [Docs] Login.js 주석 추가

--- front_week_2-6/likelion_week06/src/Login.js
+++ front_week_2-6/likelion_week06/src/Login.js
@@ -3,7 +3,9 @@ import kakao_btn_img from "./kakao_login_large_narrow.png";
 import google_btn_img from "./web_light_sq_SI@2x.png";
 
 const Login = () => {
+  // 카카오 인증을 위한 URL 주소를 입력한다. API 키와 Redirection Api는 .env에 입력된 값으로 가져온다.
   const authServerLink = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`;
+  // 카카오 로그인 버튼을 누를 시 위에서 만든 authServerLink로 리디렉션되도록 하는 함수이다.
   const handleKakao = () => {
     window.location.href = authServerLink;
   };
